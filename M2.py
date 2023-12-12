38bb177c50522c3f21f1c642d1a33c63fc35ac99
Ricardo-Avila <54330605+Ricardo-Avila@users.noreply.github.com>
2023-12-12 01:23:22 -0600

    Update M2.py

--- M2.py
+++ M2.py
@@ -1,4 +1,5 @@
 import socket
+from datetime import datetime
 
 # Dirección IP y puerto del servidor al que se conectará el cliente
 host = '192.168.183.136'
@@ -11,11 +12,18 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 print(f'Conectado al servidor en {host}:{port}')
 
-# Solicitar al usuario que ingrese un mensaje personalizado
-message = input("Ingrese su mensaje: ")
-
-# Enviar el mensaje al servidor
-s.sendall(message.encode())
+# Enviar una serie de mensajes al servidor
+messages = ["Hola, servidor!", "¿Cómo estás?", "Este es otro mensaje."]
+for message in messages:
+    # Obtener la hora actual
+    current_time = datetime.now().strftime("%H:%M:%S")
+    
+    # Crear el mensaje con la hora
+    full_message = f"[{current_time}] {message}"
+    
+    # Enviar el mensaje al servidor
+    s.sendall(full_message.encode())
+    print(f'Mensaje enviado: {full_message}')
 
 # Cerrar la conexión
 s.close()

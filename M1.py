991bb00a3378f2599b7c33cb8db7950fc04e754f
Ricardo-Avila <54330605+Ricardo-Avila@users.noreply.github.com>
2023-12-12 05:08:15 -0600

    Update M1.py

--- M1.py
+++ M1.py
@@ -1,9 +1,26 @@
 import socket
 from datetime import datetime
-import threading
 
-def manejar_conexion(conn, addr):
+# Dirección IP y puerto en el que el servidor escuchará
+host = '192.168.183.136'
+port = 12345
+
+while True:
     try:
+        # Crear un objeto socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Vincular el socket al host y puerto
+        s.bind((host, port))
+
+        # Escuchar conexiones entrantes (máximo 1 conexión en este ejemplo)
+        s.listen(1)
+
+        print(f'Esperando una conexión en {host}:{port}...')
+
+        # Aceptar la conexión entrante
+        conn, addr = s.accept()
+
         # Obtener la fecha y hora actual de la conexión
         connection_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f'Conexión establecida desde {addr} a las {connection_datetime}')
@@ -13,56 +30,29 @@ def manejar_conexion(conn, addr):
             if not data:
                 # Si la máquina2 se desconecta, mostrar el mensaje y la hora
                 disconnection_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                print(f'Máquina2 ({addr}) desconectada a las {disconnection_datetime}')
+                print(f'Máquina2 desconectada a las {disconnection_datetime}')
                 break
 
             # Mostrar el mensaje recibido
-            print(f'Datos recibidos de Máquina2 ({addr}): {data.decode()}')
+            print(f'Datos recibidos de Máquina2: {data.decode()}')
 
             # Obtener la fecha y hora actual
             current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
             # Crear el mensaje de confirmación con la fecha y hora
-            confirmation_message = f"Mensaje de Máquina2 ({addr}) recibido por el servidor el {current_datetime}."
+            confirmation_message = f"Mensaje de Máquina2 recibido por el servidor el {current_datetime}."
             conn.sendall(confirmation_message.encode())
 
-    except Exception as e:
-        print(f"Error de conexión con Máquina2 ({addr}): {e}")
-
-    finally:
-        # Cerrar la conexión después de salir del bloque try
+        # Cerrar la conexión después de salir del bucle interno
         conn.close()
 
-def iniciar_servidor():
-    # Dirección IP y puerto en el que el servidor escuchará
-    host = '192.168.183.136'
-    port = 12345
+        # Preguntar al usuario si desea reiniciar el servidor o cerrar el programa
+        user_input = input("¿Desea reiniciar el servidor? (y/n): ")
+        if user_input.lower() != 'y':
+            break  # Terminar el programa
 
-    # Crear un objeto socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except Exception as e:
+        print(f"Error de conexión: {e}")
 
-    # Vincular el socket al host y puerto
-    s.bind((host, port))
-
-    # Escuchar conexiones entrantes (máximo 5 conexiones en este ejemplo)
-    s.listen(5)
-
-    print(f'Esperando conexiones en {host}:{port}...')
-
-    try:
-        while True:
-            # Aceptar la conexión entrante
-            conn, addr = s.accept()
-
-            # Iniciar un hilo para manejar la conexión
-            threading.Thread(target=manejar_conexion, args=(conn, addr)).start()
-
-    except KeyboardInterrupt:
-        print("Servidor detenido por el usuario.")
-
-    finally:
-        # Cerrar el socket principal después de salir del bucle principal
-        s.close()
-
-# Iniciar el servidor
-iniciar_servidor()
+# Cerrar el socket principal antes de salir del bucle principal
+s.close()

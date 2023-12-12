3d73fc9cafc9811c58ec2befdf923655a7345b5b
Ricardo-Avila <54330605+Ricardo-Avila@users.noreply.github.com>
2023-12-12 01:53:03 -0600

    Maquina pierde conexion v01

--- M1.py
+++ M1.py
@@ -26,22 +26,30 @@ try:
 
     # Recibir y mostrar datos del cliente
     while True:
-        data = conn.recv(1024)
-        if not data:
-            # Si el cliente se desconecta, mostrar el mensaje y la hora
+        try:
+            data = conn.recv(1024)
+            if not data:
+                # Si el cliente se desconecta, mostrar el mensaje y la hora
+                disconnection_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                print(f'Cliente desconectado a las {disconnection_datetime}')
+                break
+
+            # Mostrar el mensaje recibido
+            print(f'Datos recibidos del cliente: {data.decode()}')
+
+            # Obtener la fecha y hora actual
+            current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+            # Crear el mensaje de confirmación con la fecha y hora
+            confirmation_message = f"Mensaje recibido por el servidor el {current_datetime}."
+            conn.sendall(confirmation_message.encode())
+
+        except ConnectionResetError:
+            # Si la conexión se reinicia abruptamente (por ejemplo, cuando se apaga la máquina cliente),
+            # mostrar el mensaje y la hora
             disconnection_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f'Cliente desconectado a las {disconnection_datetime}')
+            print(f'Cliente desconectado abruptamente a las {disconnection_datetime}')
             break
-
-        # Mostrar el mensaje recibido
-        print(f'Datos recibidos del cliente: {data.decode()}')
-
-        # Obtener la fecha y hora actual
-        current_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-        # Crear el mensaje de confirmación con la fecha y hora
-        confirmation_message = f"Mensaje recibido por el servidor el {current_datetime}."
-        conn.sendall(confirmation_message.encode())
 
 except Exception as e:
     print(f"Error: {e}")
